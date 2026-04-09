b09c931a8d6b4a6a063ec96c7c06493f9b7b322b
carolmedici <carolina.obstetriz@gmail.com>
2026-04-09 15:17:39 -0300

    refactor: update TicketController to use DTOs instead of entity

--- backend/src/main/java/com/carolmedici/helpdesk/controller/TicketController.java
+++ backend/src/main/java/com/carolmedici/helpdesk/controller/TicketController.java
@@ -1,8 +1,12 @@
 package com.carolmedici.helpdesk.controller;
 
+import com.carolmedici.helpdesk.dto.TicketRequest;
+import com.carolmedici.helpdesk.dto.TicketResponse;
 import com.carolmedici.helpdesk.entity.Ticket;
 import com.carolmedici.helpdesk.enums.TicketStatus;
+import com.carolmedici.helpdesk.mapper.TicketMapper;
 import com.carolmedici.helpdesk.service.TicketService;
+import jakarta.validation.Valid;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -22,26 +26,34 @@ public class TicketController {
 
     @PostMapping("/create-ticket")
     @PreAuthorize("hasRole('USER')")
-    public Ticket create(@RequestBody Ticket ticket, @AuthenticationPrincipal Jwt jwt){
+    public TicketResponse create(@Valid @RequestBody TicketRequest request, @AuthenticationPrincipal Jwt jwt){
+
+        Ticket ticket = TicketMapper.toEntity(request);
         String userId = jwt.getSubject();
         ticket.setUserId(userId);
 
-        return  service.create(ticket);
+        return  TicketMapper.toResponse(service.create(ticket));
     }
 
     @GetMapping("/user")
     @PreAuthorize("hasRole('USER')")
-    public List<Ticket> myTickets(@AuthenticationPrincipal Jwt jwt){
+    public List<TicketResponse> myTickets(@AuthenticationPrincipal Jwt jwt){
         String userId = jwt.getSubject();
-        return service.getByUser(userId);
+        return service.getByUser(userId).stream().map(TicketMapper::toResponse).toList();
     }
 
     @PatchMapping("/{id}/status")
     @PreAuthorize("hasRole('ADMIN')")
-    public  Ticket updaTicketatus(@PathVariable Long id, @RequestParam String status){
+    public  Ticket updateTicketStatus(@PathVariable Long id, @RequestParam String status){
         Ticket ticket = service.findById(id);
         ticket.setStatus(TicketStatus.valueOf(status));
 
         return  service.save(ticket);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping
+    public List<TicketResponse> getAll(){
+        return service.findAll().stream().map(TicketMapper::toResponse).toList();
     }
 }
