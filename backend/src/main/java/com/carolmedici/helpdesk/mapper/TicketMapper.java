632df046aa81c91fe7d8e32f37ecb07703a0576e
carolmedici <carolina.obstetriz@gmail.com>
2026-04-09 15:17:13 -0300

    feat: add TicketMapper for entity-DTO conversion

---
+++ backend/src/main/java/com/carolmedici/helpdesk/mapper/TicketMapper.java
@@ -0,0 +1,24 @@
+package com.carolmedici.helpdesk.mapper;
+
+import com.carolmedici.helpdesk.dto.TicketRequest;
+import com.carolmedici.helpdesk.dto.TicketResponse;
+import com.carolmedici.helpdesk.entity.Ticket;
+
+public class TicketMapper {
+
+    public static Ticket toEntity(TicketRequest request){
+        Ticket ticket = new Ticket();
+        ticket.setTitle(request.title());
+        ticket.setDescription(request.description());
+        return  ticket;
+    }
+
+    public static TicketResponse toResponse(Ticket ticket){
+        return  new TicketResponse(
+                ticket.getId(),
+                ticket.getTitle(),
+                ticket.getDescription(),
+                ticket.getStatus().name()
+        );
+    }
+}
