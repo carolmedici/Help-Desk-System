0e7848130db624b2316421e411ffe370f2e27387
carolmedici <carolina.obstetriz@gmail.com>
2026-04-12 23:07:36 -0300

    feat(dashboard): add ticket statistics chart

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { DashboardStats, TicketService } from '../../services/ticket.service';
 import { CommonModule } from '@angular/common';
+import { ChartModule } from 'primeng/chart';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   standalone: true,
-  imports: [CommonModule]
+  imports: [CommonModule, ChartModule]
 })
 
 export class DashboardComponent implements OnInit {
@@ -21,6 +22,9 @@ export class DashboardComponent implements OnInit {
   isLoading = true;
   userName: string = '';
 
+  chartData : any;
+  chartOptions: any;
+
   constructor(
     public authService: AuthService,
     private ticketService: TicketService
@@ -29,6 +33,7 @@ export class DashboardComponent implements OnInit {
   ngOnInit(): void {
     this.userName = this.authService.getUsername();
     this.loadDashboardData();
+    this.initChartOptions();
   }
 
   loadDashboardData(): void {
@@ -46,6 +51,7 @@ export class DashboardComponent implements OnInit {
     this.ticketService.getAdminDashboardStats().subscribe({
       next: (data) => {
         this.stats = data;
+        this.updateChart(data);
         this.isLoading = false;
       },
       error: (err) => {
@@ -55,11 +61,11 @@ export class DashboardComponent implements OnInit {
     });
   }
 
-  private getUserStats(): void {  
-    console.log('chamou')
+  private getUserStats(): void {     
     this.ticketService.getUserDashboardStats().subscribe({
       next: (data) => {
         this.stats = data;
+        this.updateChart(data);
         this.isLoading = false;
       },
       error: (err) => {
@@ -67,5 +73,38 @@ export class DashboardComponent implements OnInit {
         this.isLoading = false;
       }
     });
+  }
+
+  private initChartOptions(){
+    this.chartOptions = {
+      plugins: {
+          legend: { position: 'bottom' },
+          tooltip: {
+              callbacks: {
+                  label: (context: any) => {
+                      let label = context.label || '';
+                      let value = context.raw || 0;
+                      let total = context.dataset.data.reduce((a: number, b: number) => a + b, 0);
+                      let percentage = Math.round((value / total) * 100);
+                      return `${label}: ${value} (${percentage}%)`;
+                  }
+              }
+          }
+      },
+      cutout: '70%'
+    };
+  }
+
+  private updateChart(data: DashboardStats){
+    this.chartData = {
+      labels: ['Pending', 'Resolved'],
+      datasets: [
+        {
+          data: [data.pendingTickets, data.resolvedTickets],
+          backgroundColor: ['#FBBF24', '#10B981'],
+          hoverBackgroundColor: ['#F59E0B', '#059669']
+        }
+      ]
+    }
   }
 }
